a6c1de74b5e9ddcef491b18b2b26d8c9d862451f
Justin Campbell <Justin.Campbell92@hotmail.com>
2023-07-07 01:26:21 -0500

    initial commit

---
+++ ronnies.js
@@ -0,0 +1,7 @@
+//selecting statements
+const sloganElement = document.getElementById("slogan");
+console.log(sloganElement);
+
+sloganElement.innerText = "Best Waffles This side of the Appalachian!";
+
+sloganElement.style.fontSize = "28px";
